127fba79fa9dde36de4e2c39b76899b2cfd267be
tanyavash <tanyavashistha11@gmail.com>
2024-02-28 00:23:31 +0530

    wrong save

--- index.js
+++ index.js
@@ -14,34 +14,26 @@ app.use(async (req, res, next) => {
   try{
     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log('incoming request', ipAddress);
-    //req.clientIp = ipAddress;
 
-    await Click.create({
-        ipAddress
-    });
-
-    // next();
-
+    //check if document is present with given IP address
     let click = await Click.findOne({ ipAddress });
-    // // console.log(click);
-    // // console.log(typeof(click));  
 
+    //otherwise initialize to 1
     if(!click) {
-    // //   //console.log('Creating new document for IP address:', ipAddress);
-    click = new Click({ ipAddress: req.clientIp, count: 1 });
-    // //   await click.save();
+    click = new Click({ ipAddress, count: 1 });
+    console.log(click);
+       await click.save();
       }
-
-    // // //console.log('Updating document for IP address:', ipAddress);
+    
+    //if document is present , increment the count 
     else{ 
       click.count++;
     }
     
-
+    //save the updated document
       await click.save();
     console.log('Count incremented for IP address:', ipAddress);
 
-    //  req.clientIp = ipAddress;
      next();
   }
   catch (error){
@@ -53,12 +45,13 @@ app.use(async (req, res, next) => {
 
 app.get('/clicks', async (req, res) => {
   try{
+    //retreiving the IP address
     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    //const clientIp = req.clientIp;
     console.log(`Client IP address: ${ipAddress}`);
-
+    //finds document in database with given IP address
     const click = await Click.findOne({ ipAddress });
+    //count as same in document or otherwise 0 
     res.json({ count: click ? click.count : 0 });
   }
   catch (error) {
@@ -67,126 +60,3 @@ app.get('/clicks', async (req, res) => {
   }
 });
 app.listen( PORT, ()=> console.log(`server started at: ${PORT}`))
-
-
-
-
-// // Middleware to count visits
-// app.use(async (req, res, next) => {
-//   const visit = new Visit({ ipAddress: req.ip });
-//   await visit.save();
-//   next();
-// });
-
-// // Route to get total visit count
-// app.get('/visits', async (req, res) => {
-//   try {
-//     const totalCount = await Visit.countDocuments();
-//     res.json({ totalVisits: totalCount });
-//   } catch (error) {
-//     console.error('Error fetching visit count:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-
-
-
-// //extract IP address
-// const getClientIP = (req) => {
-// return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-// };
-
-
-
-
-// app.get('/track', async (req, res) => {
-//     // Get the client IP address
-//     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  
-//     try {
-//         // Find the visitor record for the given IP address
-//         let visitor = await Visitor.findOne({ ip });
-  
-//         // If the visitor record does not exist, create a new one
-//         if (!visitor) {
-//             visitor = new Visitor({ ip, count: 1 });
-//         } else {
-//             // Increment the visit count
-//             visitor.count++;
-//         }
-
-//         // Save the visitor record
-//         await visitor.save();
-
-//         // Send the response
-//         res.send(`Visit count for IP address ${ip}: ${visitor.count}`);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-
-// // Endpoint to track clicks
-// app.post('/track', async (req, res) => {
-//     const link = req.body.link;
-//     const ipAddress = getClientIP(req);
-  
-//     try 
-//     {
-//       if (!link) return res.status(400).json({ error: 'link not found' });
-
-  
-//       const newClick = new Click({ link, ipAddress });
-//       await newClick.save();
-  
-//       return res.json({ success: true, message: 'Click tracked' });
-//     } 
-//     catch (err)
-//     {
-//       console.error(err);
-//       res.status(400).json({ error: 'Error- not tracked' });
-//     }
-//   });
-  
-
-
-// // Endpoint to get click count for a link
-// app.post('/count', async (req, res) => {
-//     const link = req.body.link;
-  
-//     if (!link) {
-//       return res.status(400).json({ error: 'Link missing' });
-//     }
-  
-//     try {
-//       const count = await Click.countDocuments({ link });
-//       res.json({ link, count });
-//     } 
-//     catch (err) {
-//       console.error(err);
-//       res.status(400).json({ error: 'Error getting click count' });
-//     }
-//   });
-
-//     // Endpoint to visit a link
-// app.post('/visit', async (req, res) => {
-//     const link = req.body.link || req.query.link;
-
-//     try {
-//         if (!link) {
-//             return res.status(400).json({ error: 'Link missing in request body or query parameters' });
-//         }
-
-//         // Update the count for the link in your database
-//         await Click.findOneAndUpdate({ link }, { $inc: { count: 1 } });
-
-//         return res.json({ success: true, message: 'Visit tracked' });
-//     } catch (err) {
-//         console.error(err);
-//         return res.status(400).json({ error: 'Error tracking visit' });
-//     }
-// });
-
-
--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const clickSchema = new mongoose.Schema({
-        ipAddress: String,
+        ipAddress: { type: String },
         count: { type: Number, default: 0}
       });
 const Click = mongoose.model('Click', clickSchema);
